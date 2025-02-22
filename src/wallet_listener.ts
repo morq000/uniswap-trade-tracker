34194bdeeb502a9eacc70502dcfec6f504773e85
Alexander Draginsky <alexander.draginsky@gmail.com>
2025-02-22 16:10:21 +0300

    change wallet listener

--- src/wallet_listener.ts
+++ src/wallet_listener.ts
@@ -1,31 +1,82 @@
 // import { ethers } from 'ethers';
-import 'dotenv/config';
-import {
-  CHAIN_ID,
-  CHAIN_NAMES,
-  TARGET_WALLET_ADDRESS,
-} from './config';
+import "dotenv/config";
+import { CHAIN_ID, CHAIN_NAMES, TARGET_WALLET_ADDRESS } from "./config";
 // import { provider } from './utils/web3Provider';
-import { sendTelegramMessage } from './telegram_notifier';
-import { graphQlV3Client, graphQlV2Client, getWalletV3Swaps, getWalletv2Swaps } from './graphql/graphql';
+import { sendTelegramMessage } from "./telegram_notifier";
+import {
+    graphQlV3Client,
+    graphQlV2Client,
+    getWalletV3Swaps,
+    getWalletv2Swaps,
+    V2SwapsResponse,
+    V3SwapsResponse,
+    V3Swap,
+    V2Swap,
+} from "./graphql/graphql";
 
 // // Временное хранилище для транзакций
-// const transactionPool: ethers.TransactionResponse[] = [];
-const lastTimestamp = 1738273319;
+const transactionPool: (V3Swap | V2Swap)[] = [];
+let lastTimestamp = Math.floor(Date.now() / 1000); // Текущее время в секундах //1738273319//
 
 // Функция мониторинга
 export const startWalletMonitoring = async () => {
-  await sendTelegramMessage(
-    `Начало мониторинга кошелька ${TARGET_WALLET_ADDRESS} в сети ${CHAIN_NAMES[CHAIN_ID]}`,
-  );
+    await sendTelegramMessage(
+        `Начало мониторинга кошелька ${TARGET_WALLET_ADDRESS} в сети ${CHAIN_NAMES[CHAIN_ID]}`
+    );
+    // Основная петля приложения
+    setInterval(async () => {
+        try {
+            // Запрос свопов из V3
+            const v3Result = await graphQlV3Client.request<V3SwapsResponse>(
+                getWalletV3Swaps,
+                {
+                    origin: TARGET_WALLET_ADDRESS,
+                    timestamp_gt: lastTimestamp,
+                }
+            );
 
-  // Основная петля приложения
-  setInterval(async () => {
-    const result = await graphQlV3Client.request(getWalletV3Swaps, {origin: TARGET_WALLET_ADDRESS, timestamp_gt: lastTimestamp});
-    console.log('Query V3 result: ', result);
-    const v2Result = await graphQlV2Client.request(getWalletv2Swaps, {from: TARGET_WALLET_ADDRESS, timestamp_gt: lastTimestamp});
-    console.log('V2 result: ', v2Result);
-  }, 2000);
+            // Запрос свопов из V2
+            const v2Result = await graphQlV2Client.request<V2SwapsResponse>(
+                getWalletv2Swaps,
+                {
+                    from: TARGET_WALLET_ADDRESS,
+                    timestamp_gt: lastTimestamp,
+                }
+            );
+
+            // Объединение результатов
+            const allSwaps = [...v3Result.swaps, ...v2Result.swaps];
+            console.log("New swaps: ", allSwaps);
+
+            if (allSwaps.length > 0) {
+                // Сортировка свопов по временной метке в порядке возрастания
+                allSwaps.sort(
+                    (a, b) => parseInt(a.timestamp) - parseInt(b.timestamp)
+                );
+
+                // Добавление новых свопов в пул транзакций
+                transactionPool.push(...allSwaps);
+                console.log("TX pool: ", transactionPool);
+                // Обновление lastTimestamp на временную метку последнего свопа
+                lastTimestamp = parseInt(
+                    allSwaps[allSwaps.length - 1].timestamp
+                );
+
+                // Обработка новых свопов (например, отправка уведомлений)
+                for (const swap of allSwaps) {
+                    await sendTelegramMessage(
+                        `Новый своп: ${JSON.stringify(swap)}`
+                    );
+                }
+            } else {
+                // Если новых свопов нет, обновляем lastTimestamp на текущее время
+                lastTimestamp = Math.floor(Date.now() / 1000);
+            }
+            console.log("New timestamp: ", lastTimestamp);
+        } catch (error) {
+            console.error("Ошибка при получении свопов:", error);
+        }
+    }, 10000);
 };
 
 // // Функция для обработки транзакций из хранилища
